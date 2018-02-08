a7c415866186be571d081503c9a6ed657cb75fe5
cocokiri <strasser.ms@gmail.com>
2018-02-08 14:48:36 +0100

    adding logic for extension in build

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ import { request } from 'graphql-request'
 
 const query = `{hello(name:"Hackmind"){message}}`
 
+
 request('https://api.graph.cool/simple/v1/cjdd66m810f1s0165fe0efssz', query).then(data => console.log(data))
+
 
 
 // console.log(graphtest)
